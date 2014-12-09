2caeccb15ea9ff9a63e3980f235683183516c78e
Jan Boonen <jan.boonen@geodan.nl>
2014-12-09 16:14:14 +0100

    Initial commit of Spring Boot Admin project (including Dockerfile).

---
+++ microservices-admin/src/main/java/com/geodan/labs/microservices/AdminApplication.java
@@ -0,0 +1,26 @@
+package com.geodan.labs.microservices;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@EnableAdminServer
+@EnableAutoConfiguration
+@ImportResource({"classpath:hazelcast-config.xml"})
+public class AdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminApplication.class);
+    }
+
+}
